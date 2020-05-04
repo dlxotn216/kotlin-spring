54a6ac26b951e07a9a306442752f2f0f775df502
Lee Tae Su <taesu@crscube.co.kr>
2020-05-04 13:45:43 +0900

    Study2_3
    Property, Getter, Setter, Readonly property

--- src/main/kotlin/me/taesu/demo/chap02/Study2_1.kt
+++ src/main/kotlin/me/taesu/demo/chap02/Study2_1.kt
@@ -7,74 +7,76 @@ package me.taesu.demo.chap02
  * @version 1.2.0
  * @since 1.2.0
  */
-data class Person(val name: String, var age: Int? = null);
-
 fun main(args: Array<String>) {
-    exam01()
-    exam02()
-    exam03()
+    Study2_1().exam01()
+    Study2_1().exam02()
+    Study2_1().exam03()
 }
 
-private fun exam01() {
-    val oldestPerson = listOf<Person>(Person("Lee Tae Su", 28),
-            Person("Lee Tae Su 2"),
-            Person("Tae Su Lee", 30),
-            Person("Yoon So young", 27))
-            .maxBy {
-                /* person -> if (person.age == null) 0 else person.age  */          // 허용 되지 않음
-                /* person -> person.age == null ? 0 : person.age */                 // 허용 되지 않음
-                person ->
-                person.age ?: 0
-            }
+class Study2_1 {
+    private data class Person(val name: String, var age: Int? = null);
 
-    println("oldest peron is $oldestPerson")
-}
+    fun exam01() {
+        val oldestPerson = listOf<Person>(Person("Lee Tae Su", 28),
+                Person("Lee Tae Su 2"),
+                Person("Tae Su Lee", 30),
+                Person("Yoon So young", 27))
+                .maxBy {
+                    /* person -> if (person.age == null) 0 else person.age  */          // 허용 되지 않음
+                    /* person -> person.age == null ? 0 : person.age */                 // 허용 되지 않음
+                    person ->
+                    person.age ?: 0
+                }
 
-private fun exam02() {
-    val oldestPerson = listOf<Person>(Person("Lee Tae Su", 28),
-            Person("Lee Tae Su 2"),
-            Person("Tae Su Lee", 30),
-            Person("Yoon So young", 27))
-            .maxBy { it.age ?: 0 }      //it으로 간소화 할 수 있음
+        println("oldest peron is $oldestPerson")
+    }
 
-    println("oldest peron is $oldestPerson")
-}
+    fun exam02() {
+        val oldestPerson = listOf<Person>(Person("Lee Tae Su", 28),
+                Person("Lee Tae Su 2"),
+                Person("Tae Su Lee", 30),
+                Person("Yoon So young", 27))
+                .maxBy { it.age ?: 0 }      //it으로 간소화 할 수 있음
 
-private fun exam03() {
-    //Compile error가 나지 않음
-    val findPersonByName = findPersonByName(listOf(Person("Lee Tae Su", 28),
-            Person("Lee Tae Su 2"),
-            Person("Tae Su Lee", 30),
-            Person("Yoon So young", 27)), "Lee Tae Su") ?: Person("Unknown")
+        println("oldest peron is $oldestPerson")
+    }
 
-    val findPerson = findPerson(listOf(Person("Lee Tae Su", 28),
-            Person("Lee Tae Su 2"),
-            Person("Tae Su Lee", 30),
-            Person("Yoon So young", 27)), { person -> person.name == "Lee Tae Su 22" })
+    fun exam03() {
+        //Compile error가 나지 않음
+        val findPersonByName = findPersonByName(listOf(Person("Lee Tae Su", 28),
+                Person("Lee Tae Su 2"),
+                Person("Tae Su Lee", 30),
+                Person("Yoon So young", 27)), "Lee Tae Su") ?: Person("Unknown")
 
-    println("fins person by name is $findPersonByName")
-    println("fins person is $findPerson")
+        val findPerson = findPerson(listOf(Person("Lee Tae Su", 28),
+                Person("Lee Tae Su 2"),
+                Person("Tae Su Lee", 30),
+                Person("Yoon So young", 27)), { person -> person.name == "Lee Tae Su 22" })
 
-    //Compile error -> 명확히 타입을 선언해주는 것이 좋을 것
-    /*
-    val test: Person = findPersonByName(listOf(Person("Lee Tae Su", 28),
-            Person("Lee Tae Su 2"),
-            Person("Tae Su Lee", 30),
-            Person("Yoon So young", 27)), "Lee Tae Su")
-    */
+        println("fins person by name is $findPersonByName")
+        println("fins person is $findPerson")
 
-    val test2: Person? = findPersonByName(listOf(Person("Lee Tae Su", 28),
-            Person("Lee Tae Su 2"),
-            Person("Tae Su Lee", 30),
-            Person("Yoon So young", 27)), "Lee Tae Su")
-}
+        //Compile error -> 명확히 타입을 선언해주는 것이 좋을 것
+        /*
+        val test: Person = findPersonByName(listOf(Person("Lee Tae Su", 28),
+                Person("Lee Tae Su 2"),
+                Person("Tae Su Lee", 30),
+                Person("Yoon So young", 27)), "Lee Tae Su")
+        */
 
-// Nullable Person을 반환
-private fun findPersonByName(persons: List<Person>, name: String): Person? {
-    return persons.firstOrNull { person -> person.name == name }
-}
+        val test2: Person? = findPersonByName(listOf(Person("Lee Tae Su", 28),
+                Person("Lee Tae Su 2"),
+                Person("Tae Su Lee", 30),
+                Person("Yoon So young", 27)), "Lee Tae Su")
+    }
 
-// Null safety Person을 반환
-private fun findPerson(persons: List<Person>, matcher: (Person) -> Boolean): Person {
-    return persons.firstOrNull(matcher) ?: Person("Unknown")
+    // Nullable Person을 반환
+    private fun findPersonByName(persons: List<Person>, name: String): Person? {
+        return persons.firstOrNull { person -> person.name == name }
+    }
+
+    // Null safety Person을 반환
+    private fun findPerson(persons: List<Person>, matcher: (Person) -> Boolean): Person {
+        return persons.firstOrNull(matcher) ?: Person("Unknown")
+    }
 }
---
+++ src/main/kotlin/me/taesu/demo/chap02/Study2_3.kt
@@ -0,0 +1,69 @@
+package me.taesu.demo.chap02
+
+/**
+ * Created by itaesu on 04/05/2020.
+ *
+ * @author Lee Tae Su
+ * @version 1.2.0
+ * @since 1.2.0
+ */
+fun main(args: Array<String>) {
+    Study2_3().exam1()
+    Study2_3().exam2()
+}
+
+class Study2_3 {
+    private data class Person(val name: String,     // readonly -> only getter
+                              var isMarried: Boolean = false
+            // getter + setter
+            // is로 시작하는 프로퍼티는 isMarried(), setMarried(married)로 생성이 된다.
+    )
+
+    private data class Rectangle(var width: Int,
+                                 var height: Int) {
+
+        //custom accessor
+        val isSquare: Boolean
+            get() {
+                return width == height
+            }
+
+        //위와 동일한 표현
+        val isSquare2: Boolean get() = width == height
+
+        //이 표현은 함수가 아님을 주의
+        val isSquare3: Boolean = width == height
+
+        //cannot have set
+//        set(isSqure: Boolean) {
+//
+//        }
+    }
+
+
+    fun exam1() {
+        val taesu = Person("Lee Tae Su")
+        taesu.isMarried = false
+
+        println(taesu);
+    }
+
+    fun exam2() {
+        println(Rectangle(2, 4).isSquare)
+
+        println()
+        val square2 = Rectangle(4, 4)
+        println("${square2} is square ? ${square2.isSquare2}")
+
+        square2.width = 5
+        println("${square2} is square ? ${square2.isSquare2}")
+
+        println()
+        val square3 = Rectangle(4, 4)
+        println("${square3} is square ? ${square3.isSquare}")
+
+        square3.width = 5
+        println("${square3} is square ? ${square3.isSquare}")
+
+    }
+}
