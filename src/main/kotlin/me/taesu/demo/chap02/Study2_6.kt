d9ece8c5c6eff015dfbc1fd3d4fc65bf506bcf31
Lee Tae Su <taesu@crscube.co.kr>
2020-05-04 16:42:37 +0900

    Study2_6
    Iteration
    Range
    
    폐구간, 열린구간
    
    until, step, for in

---
+++ src/main/kotlin/me/taesu/demo/chap02/Study2_6.kt
@@ -0,0 +1,55 @@
+package me.taesu.demo.chap02
+
+/**
+ * Created by itaesu on 04/05/2020.
+ *
+ * @author Lee Tae Su
+ * @version 1.2.0
+ * @since 1.2.0
+ */
+fun main(args: Array<String>) {
+    Study2_6().exam1()
+}
+
+class Study2_6 {
+    fun fizzBuzz(i: Int) {
+        when {
+            i % 15 == 0 -> println("FizzBuzz")
+            i % 3 == 0 -> println("Fizz")
+            i % 5 == 0 -> println("Buzz")
+            else -> println(i)
+        }
+    }
+
+    fun exam1() {
+        //폐구간 (닫힌 구간) -> 양끝을 다 포함 함
+        for (i in 1..100) {
+            fizzBuzz(i)
+        }
+
+        println()
+        //폐구간 (닫힌 구간) -> 양끝을 다 포함 함
+        val intProgression = 100 downTo 1 step 2// 2씩 감소하는 역방향 수열
+        for (i in 100 downTo 1 step 2) {
+            fizzBuzz(i)
+        }
+
+        //열린 구간 for(i=0; i<100; i++)
+        println("i in 0 until 100")
+        for(i in 0 until 100){
+            print("$i ")
+        }
+
+        //열린 구간 for(i=0; i<100; i++)
+        println("i in 0..100-1")
+        for(i in 0..100-1){
+            print("$i ")
+        }
+
+        //for (i=0; i< 100; i+=2)
+        println("i in 0 until 100 step 2")
+        for(i in 0 until 100 step 2){
+            print("$i ")
+        }
+    }
+}
