8029516ff758bd82187ddcb9f93cb64f02275228
Lee TaeSu <taesu@crscube.co.kr>
2020-05-06 23:11:27 +0900

    Study2_8
    try-catch-finally를 식으로
    (try-with-resource는 별도로 제공하지 않으며 라이브러리 형태로 사용 가능)
    
    return을 쓸 수 없는 식이 본문인 함수와
    return을 써야하는 블록이 본문인 함수

---
+++ src/main/kotlin/me/taesu/demo/chap02/Study2_8.kt
@@ -0,0 +1,65 @@
+package me.taesu.demo.chap02
+
+import java.io.BufferedReader
+import java.io.StringReader
+
+/**
+ * Created by taesu on 2020-05-06.
+ */
+
+fun main(args: Array<String>) {
+    Study2_8().exam1();
+}
+
+class Study2_8 {
+
+    fun exam1() {
+        val number = 100
+        val percentage: Int =
+                if (number in 0..100){
+                    number
+                } else {
+                    throw IllegalArgumentException("$number is not a percentage")
+                }
+
+        fun readNumber(reader: BufferedReader): Int? {
+            try {
+                val line = reader.readLine()
+                return Integer.parseInt(line)
+            } catch (e: Exception){
+                return null
+            } finally {
+                reader.close()
+            }
+        }
+
+        //Kotlin은 모두다 Unchecked Exception임
+        println(readNumber(BufferedReader(StringReader("239"))))
+
+        //블록이 본문인 함수라면 return이 필요.
+        fun tryExpr(reader: BufferedReader): Int? {
+            val input = try {
+                Integer.parseInt(reader.readLine())
+            } catch (e: Exception){
+                null
+            } finally {
+                reader.close()
+            }
+            return input
+        }
+        println(tryExpr(BufferedReader(StringReader("awef"))))
+
+        //식이 본문인 함수라면 return 없어야 함.
+        fun returnTryCatch(reader: BufferedReader): Int? =
+                try {
+                    Integer.parseInt(reader.readLine())
+                } catch (e: Exception){
+                    null
+                } finally {
+                    reader.close()
+                }
+
+        println(returnTryCatch(BufferedReader(StringReader("awef"))))
+        println(returnTryCatch(BufferedReader(StringReader("234324"))))
+    }
+}
