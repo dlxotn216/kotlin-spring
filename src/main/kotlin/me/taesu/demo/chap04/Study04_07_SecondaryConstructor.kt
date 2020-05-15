c329d9175764a81cc2172b82c0ae5bdb328d6486
Lee TaeSu <taesu@crscube.co.kr>
2020-05-16 01:56:06 +0900

    Study04_07
    부 생성자
    - 클래스 본문 내부에 constructor로 선언된 생성자
    
    this로 또 다른 생성자 호출 할 수 있음
    constructor(name: String) : this(name, null)
    constructor(name: String, email: String?)
    
    상위클래스의 주 생성자가 호출 안되다면 아래와 같이
    상위클래스의 생성자에 위임해야 함
    constructor(name: String) : super(name)
    
    웬만해선 부 생성자를 선언하지 말 것
    -> 자바 호환을 위한 것일 뿐
    
    아래와 같이 디폴트값을 주 생성자에 설정하자
    class User(val name: String, var email: String?=null)

---
+++ src/main/kotlin/me/taesu/demo/chap04/Study04_07_SecondaryConstructor.kt
@@ -0,0 +1,51 @@
+package me.taesu.demo.chap04
+
+/**
+ * Created by taesu on 2020-05-16.
+ */
+
+fun main(args: Array<String>) {
+    Study04_07_SecondaryConstructor().exam()
+}
+
+class Study04_07_SecondaryConstructor {
+    private open class User {
+        val name: String
+        var email: String?
+
+        //주 생성자 외의 생성자를 부 생성자라고 한다
+        //아래와 같이 다른 부 생성자를 호출할 수 있다
+        constructor(name: String) : this(name, null)
+
+
+        constructor(name: String, email: String?) {
+            this.name = name
+            this.email = email
+        }
+    }
+
+    private class User1 : User {
+        constructor(name: String) : super(name)
+
+        constructor(name: String, email: String?) : super(name, email)
+    }
+
+    private class User2(name: String) : User(name) {
+        constructor(name: String, email: String?) : this(name)
+
+        //아래와 같이는 불가. primary constructor call expected
+//        constructor(name: String, email: String?) : super(name)
+    }
+
+    //웬만해선 부 생성자를 만들지 말고 아래와 같이 기본값을 줄 것
+    //부 생성자는 자바 상호운용성 때문에 있는 것이다
+    private open class User3(val name: String, var email: String? = null)
+
+    private class User4(name: String) : User3(name)
+    private class User5(name: String, email: String?) : User3(name, email)
+
+    fun exam() {
+        println(User("lee").name)
+        println(User("lee").email)
+    }
+}
