8837cc63c74b6d17e6b7297b1bb5d026fbd569f8
Lee Tae Su <taesu@crscube.co.kr>
2020-05-22 08:56:32 +0900

    Study05_02 Lambda Capturing, Method reference
    람다에선 외부의 final이 아닌 변수를 변경할 수 있다
    -> 메소드 내에서 람다가 반환되면 참조된(포획된) 변수의 생명주기가 달라진다
    
    this::age
    ::age
    Men::age
    
    와 같이 메소드 참조를 통해 함수 자체를 람다로 가져올 수 있다

---
+++ src/main/kotlin/me/taesu/demo/chap05/Study05_02_LambdaVariableCapture_MethodReference.kt
@@ -0,0 +1,55 @@
+package me.taesu.demo.chap05
+
+/**
+ * Created by itaesu on 22/05/2020.
+ *
+ * @author Lee Tae Su
+ * @version 1.2.0
+ * @since 1.2.0
+ */
+fun main(args: Array<String>) {
+    Study05_02LambdaWithVariable().exam(listOf("404", "500"))
+    Study05_02LambdaWithVariable().exam2()
+}
+
+class Study05_02LambdaWithVariable {
+    fun exam(response: List<String>) {
+        var clientErrors = 0
+        var serverErrors = 0
+
+        //java와 달리 람다 안에서 변수를 capturing 할 수 있다.
+        //만약 람다를 반환하는 경우 위 값들을 따로 래핑하여 사용함
+        response.forEach {
+            if (it.startsWith("4")) {
+                clientErrors++
+            } else {
+                serverErrors++
+            }
+        }
+
+        println(clientErrors)
+        println(serverErrors)
+    }
+
+    private open class Human(val sex: String)
+
+    private data class Men(val name: String, val age: Int) : Human("M") {
+
+        //this는 생략 가능
+        private val getNameFunc1 = this::name
+        private val getNameFunc2 = ::name
+        private val getSexFunc = ::sex
+
+        private val constructorRef = ::Men
+    }
+
+    fun exam2() {
+        val getAge = Men::age
+
+        println(getAge.get(Men("taesu", 28)))
+
+        println(listOf(Men("taesu", 28), Men("tatoo", 18), Men("spec", 38)).maxBy(getAge))
+
+
+    }
+}
