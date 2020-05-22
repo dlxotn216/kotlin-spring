8d96e08338b89a26c873130e36567417cb1504ba
Lee Tae Su <taesu@crscube.co.kr>
2020-05-22 09:14:34 +0900

    Study05_03 Collection API (filter, map)
    Filter는 요소를 걸러내는 역할한다
    
    Map은 컨테이너의 추상화 수준을 바꾸는 역할을 한다
    
    - people.map { it.name } ==>>> <Person> -> <String>

---
+++ src/main/kotlin/me/taesu/demo/chap05/Study05_03_CollectionAPI_Filter_Map.kt
@@ -0,0 +1,28 @@
+package me.taesu.demo.chap05
+
+/**
+ * Created by itaesu on 22/05/2020.
+ *
+ * @author Lee Tae Su
+ * @version 1.2.0
+ * @since 1.2.0
+ */
+fun main(args: Array<String>) {
+    Study05_03_CollectionAPI_Filter_Map().exam()
+}
+
+class Study05_03_CollectionAPI_Filter_Map {
+    private data class Person(val name: String, val age: Int)
+
+    fun exam() {
+        println(listOf(1, 2, 3, 4, 5, 6, 7, 8).filter { it % 2 == 0 })
+        println(listOf(Person("young men", 18), Person("taesu", 28), Person("old men", 50)).filter { it.age > 30 })
+
+        println(listOf(1, 2, 3, 4, 5, 6, 7, 8).map { it * it })
+        println(listOf(Person("young men", 18), Person("taesu", 28), Person("old men", 50)).map { it.name })
+
+        println(listOf(Person("young men", 18), Person("taesu", 28), Person("old men", 50))
+                .filter { it.age > 30 }.map { it.name })
+
+    }
+}
