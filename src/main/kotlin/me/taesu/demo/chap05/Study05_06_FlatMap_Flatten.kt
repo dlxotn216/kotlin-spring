df9d170365f0db01d5afa7d8d52c14b9c53005aa
Lee Tae Su <taesu@crscube.co.kr>
2020-05-25 17:20:10 +0900

    Study05_06 FlatMap, Flatten
    data class book(val name: String, val authors: List<String)의 list인 경우
    
    books.flatMap{ it.authors }.toSet() => list of authors
    1. Map:  모든 book을 authros로 변환 (List<Book> => List<List<String>>)
    2. Flatten:  컬렉션을 펼치기 (List<List<String>> => List<String>)
    
    이 때 별도의 map에서 변환과정이 필요 없다면 flatten 사용 가능
    books.flatten()

---
+++ src/main/kotlin/me/taesu/demo/chap05/Study05_06_FlatMap_Flatten.kt
@@ -0,0 +1,33 @@
+package me.taesu.demo.chap05
+
+/**
+ * Created by itaesu on 25/05/2020.
+ *
+ * @author Lee Tae Su
+ * @version 1.2.0
+ * @since 1.2.0
+ */
+fun main(args: Array<String>) {
+    Study05_06_FlatMap_Flatten().exam()
+}
+
+class Study05_06_FlatMap_Flatten {
+    private data class Book(val name: String, val authors: List<String>)
+
+    fun exam(){
+        println(listOf(Book("Kotlin in action", listOf("taesu", "lee", "kim")),
+                Book("java in action", listOf("yoon", "tim", "Merry")),
+                Book("lambda in action", listOf("class", "lee", "taesu")))
+                .flatMap { it.authors }     //1. 반복하며 authors로 변환한다 (map(, 2. 하나의 컬렉션으로 합친다 (flatten)
+                .toSet())
+
+        val listOfList = listOf(listOf("taesu", "lee", "kim"),
+                listOf("qwer", "qqq", "eefawef"),
+                listOf("123123", "wef", "kim"))
+
+        println(listOfList.flatMap { it })
+
+        //same with flatMap{ it }
+        println(listOfList.flatten())
+    }
+}
