480230cded2fff1613e1ff3c9fb4d352f25cc6d7
Lee TaeSu <taesu@crscube.co.kr>
2020-05-26 23:39:48 +0900

    Study05_07_LazyCollection
    Collection을 대상으로 하는 map, filter 연산은
    매선 새로운 Collection을 반환한다.
    
    Sequence는 한 번에 하나의 요소를 말하는 인터페이스로
    Lazy 연산이 적용된다.
    
    필요 시점에 직접저인 연산을 하며, 중간 연산 결과를 따로 저장하지않고
     연산을 연쇄족으로 적용하여 Collection을 반환한다는 의미이다.

---
+++ src/main/kotlin/me/taesu/demo/chap05/Study05_07_LazyCollection.kt
@@ -0,0 +1,42 @@
+package me.taesu.demo.chap05
+
+/**
+ * Created by taesu on 2020-05-26.
+ */
+
+fun main(args: Array<String>) {
+    Study05_07_LazyCollection().exam()
+}
+
+class Study05_07_LazyCollection {
+    private data class Person(val name: String)
+
+    fun exam() {
+        val filter = listOf(Person("taesu"),
+                Person("aef"),
+                Person("awefwaf"),
+                Person("Kim"),
+                Person("yoon"),
+                Person("Lee"),
+                Person("You"),
+                Person("Park"))                      //  List<Person>
+                .map { it.name }                            //  List<String>
+                .filter { it.startsWith("K") }       //  Filtered List<String>
+        println(filter)
+
+
+        val filter2 = listOf(Person("taesu"),
+                Person("aef"),
+                Person("awefwaf"),
+                Person("Kim"),
+                Person("yoon"),
+                Person("Lee"),
+                Person("You"),
+                Person("Park"))                          // List<Person>
+                .asSequence()
+                .map { it.name }
+                .filter { it.startsWith("K") }
+                .toList()                                       // Filtered List<String>
+        println(filter2)
+    }
+}
