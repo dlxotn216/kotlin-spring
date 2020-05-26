2d2ec454aa94a8a90a2d48b3f025c30c65b429b4
Lee TaeSu <taesu@crscube.co.kr>
2020-05-27 00:37:58 +0900

    Study05_07_LazyCollection2
    Filter와 Map의 순서
    
    일반적으로 map 후 filter를 하면 필요 없는 대상에 대해서도 map 연산이들어간다
    
    filter 연산을 통해 필요한요소들에 대해서만 map 하면 연산 횟수를 줄일 수 있다.
    
    이것은 collection, sequence 둘다 마찬가지 이다.
    
    [map -> filter]
    
    A    B    C    D
    |    |    |    |
    A`   B`   C`   D`
    |    |    |    |
    X    B`   X    D`
    
    filter -> map
    
    A    B    C    D
    |    |    |    |
    X    B`   X    D`
         |         |
         B`        D`

--- src/main/kotlin/me/taesu/demo/chap05/Study05_07_LazyCollection2.kt
+++ src/main/kotlin/me/taesu/demo/chap05/Study05_07_LazyCollection2.kt
@@ -116,5 +116,165 @@ class Study05_07_LazyCollection2 {
         find -> 4
         4
          */
+
+
+        println()
+        //이름의 길이가 3 보다 큰 사람 이름 출력 (Map -> filter)
+        val namesLonggerthanThree1 = listOf(Person("taesu"),
+                Person("aef"),
+                Person("awefwaf"),
+                Person("Kim"),
+                Person("yoon"),
+                Person("Lee"),
+                Person("You"),
+                Person("Park"))
+                .map {
+                    println("""namesLonggerthanThree1 >>> map $it""")
+                    it.name
+                }
+                .filter {
+                    println("""namesLonggerthanThree1 >>> filter $it""")
+                    it.length > 3
+                }
+                .toList()
+        println(namesLonggerthanThree1)
+        /*
+        namesLonggerthanThree1 >>> map Person(name=taesu)
+        namesLonggerthanThree1 >>> map Person(name=aef)
+        namesLonggerthanThree1 >>> map Person(name=awefwaf)
+        namesLonggerthanThree1 >>> map Person(name=Kim)
+        namesLonggerthanThree1 >>> map Person(name=yoon)
+        namesLonggerthanThree1 >>> map Person(name=Lee)
+        namesLonggerthanThree1 >>> map Person(name=You)
+        namesLonggerthanThree1 >>> map Person(name=Park)
+        namesLonggerthanThree1 >>> filter taesu
+        namesLonggerthanThree1 >>> filter aef
+        namesLonggerthanThree1 >>> filter awefwaf
+        namesLonggerthanThree1 >>> filter Kim
+        namesLonggerthanThree1 >>> filter yoon
+        namesLonggerthanThree1 >>> filter Lee
+        namesLonggerthanThree1 >>> filter You
+        namesLonggerthanThree1 >>> filter Park
+        [taesu, awefwaf, yoon, Park]
+         */
+
+        println()
+        //이름의 길이가 3 보다 큰 사람 이름 출력 (Filter -> Map)
+        val namesLonggerthanThree2 = listOf(Person("taesu"),
+                Person("aef"),
+                Person("awefwaf"),
+                Person("Kim"),
+                Person("yoon"),
+                Person("Lee"),
+                Person("You"),
+                Person("Park"))
+                .filter {
+                    println("""namesLonggerthanThree2 >>> filter $it""")
+                    it.name.length > 3
+                }
+                .map {
+                    println("""namesLonggerthanThree2 >>> map $it""")
+                    it.name
+                }
+                .toList()
+        println(namesLonggerthanThree2)
+
+        /*
+        namesLonggerthanThree2 >>> filter Person(name=taesu)
+        namesLonggerthanThree2 >>> filter Person(name=aef)
+        namesLonggerthanThree2 >>> filter Person(name=awefwaf)
+        namesLonggerthanThree2 >>> filter Person(name=Kim)
+        namesLonggerthanThree2 >>> filter Person(name=yoon)
+        namesLonggerthanThree2 >>> filter Person(name=Lee)
+        namesLonggerthanThree2 >>> filter Person(name=You)
+        namesLonggerthanThree2 >>> filter Person(name=Park)
+
+        Filter 된 것들에 대해서만 Map을 처리한다. -> 연산회수 감소.
+        namesLonggerthanThree2 >>> map Person(name=taesu)
+        namesLonggerthanThree2 >>> map Person(name=awefwaf)
+        namesLonggerthanThree2 >>> map Person(name=yoon)
+        namesLonggerthanThree2 >>> map Person(name=Park)
+        [taesu, awefwaf, yoon, Park]
+         */
+
+
+        println()
+        //이름의 길이가 3 보다 큰 사람 이름 출력 (Map -> filter), Sequence
+        val namesLonggerthanThree3 = listOf(Person("taesu"),
+                Person("aef"),
+                Person("awefwaf"),
+                Person("Kim"),
+                Person("yoon"),
+                Person("Lee"),
+                Person("You"),
+                Person("Park"))
+                .asSequence()
+                .map {
+                    println("""namesLonggerthanThree3 >>> map $it""")
+                    it.name
+                }
+                .filter {
+                    println("""namesLonggerthanThree3 >>> filter $it""")
+                    it.length > 3
+                }
+                .toList()
+        println(namesLonggerthanThree3)
+        /*
+        namesLonggerthanThree3 >>> map Person(name=taesu)
+        namesLonggerthanThree3 >>> filter taesu
+        namesLonggerthanThree3 >>> map Person(name=aef)
+        namesLonggerthanThree3 >>> filter aef
+        namesLonggerthanThree3 >>> map Person(name=awefwaf)
+        namesLonggerthanThree3 >>> filter awefwaf
+        namesLonggerthanThree3 >>> map Person(name=Kim)
+        namesLonggerthanThree3 >>> filter Kim
+        namesLonggerthanThree3 >>> map Person(name=yoon)
+        namesLonggerthanThree3 >>> filter yoon
+        namesLonggerthanThree3 >>> map Person(name=Lee)
+        namesLonggerthanThree3 >>> filter Lee
+        namesLonggerthanThree3 >>> map Person(name=You)
+        namesLonggerthanThree3 >>> filter You
+        namesLonggerthanThree3 >>> map Person(name=Park)
+        namesLonggerthanThree3 >>> filter Park
+        [taesu, awefwaf, yoon, Park]
+         */
+
+
+        println()
+        //이름의 길이가 3 보다 큰 사람 이름 출력 (Filter -> Map), Sequence
+        val namesLonggerthanThree4 = listOf(Person("taesu"),
+                Person("aef"),
+                Person("awefwaf"),
+                Person("Kim"),
+                Person("yoon"),
+                Person("Lee"),
+                Person("You"),
+                Person("Park"))
+                .asSequence()
+                .filter {
+                    println("""namesLonggerthanThree4 >>> filter $it""")
+                    it.name.length > 3
+                }
+                .map {
+                    println("""namesLonggerthanThree4 >>> map $it""")
+                    it.name
+                }
+                .toList()
+        println(namesLonggerthanThree4)
+        /*
+        namesLonggerthanThree4 >>> filter Person(name=taesu)
+        namesLonggerthanThree4 >>> map Person(name=taesu)
+        namesLonggerthanThree4 >>> filter Person(name=aef)
+        namesLonggerthanThree4 >>> filter Person(name=awefwaf)
+        namesLonggerthanThree4 >>> map Person(name=awefwaf)
+        namesLonggerthanThree4 >>> filter Person(name=Kim)
+        namesLonggerthanThree4 >>> filter Person(name=yoon)
+        namesLonggerthanThree4 >>> map Person(name=yoon)
+        namesLonggerthanThree4 >>> filter Person(name=Lee)
+        namesLonggerthanThree4 >>> filter Person(name=You)
+        namesLonggerthanThree4 >>> filter Person(name=Park)
+        namesLonggerthanThree4 >>> map Person(name=Park)
+        [taesu, awefwaf, yoon, Park]
+         */
     }
 }
