756f0974e133a54d44499a06b61ab22ce0341753
Lee Tae Su <taesu@crscube.co.kr>
2020-05-28 12:10:27 +0900

    Study05_08 Lambda
    - FunctionalInterface에 람다 전달하기
    
    fun pass1(runnable: Runnable) 인 경우
          pass1(Runnable {println("test")}) 와 같이 감싸주어야 한다
    
    private data class User(val name: String)
    
        private fun pass2(name: String,
                          supplier: (name: String) -> User): User = supplier.invoke(name)
    
    - Lambda 선언에 람다 전달하기
    
    private data class User(val name: String)
    private fun pass2(name: String,
                      supplier: (name: String) -> User ): User = supplier.invoke(name)
    
          pass2("taesu", { User(it)})
          pass2("taesu", { name: String -> User(name) }))
    
    만약 Lambda가 로컬 변수와 같이 외부에 선언된 것을 참조하고 있다면 (Capture)
    람다는 매번 호출시 마다 새로 생성된다.
    
    하지만 아무것도 포획하지 않는다면 전역에 하나만 생성되어 공유된다.
    
    pass2 메소드는 무명객체로 못넘긴다. 무명객체를 생성할 수 있는 인터페이스가 없기때문.
    
    아래와 같이 pass3 메소드인 경우는 UserSupplier 인터페이스에 대해 무명 객체를 생성하여 넘길 수 있으며
    이 경우엔 람다와 달리 매번 호출마다 새로운 무명객체 가 생성된다
    private fun pass3(name: String, supplier: UserSupplier) = supplier.supply(name)
    
        private interface UserSupplier {
            fun supply(name: String): User
        }
    
        pass3("taesu", object : UserSupplier {
                    override fun supply(name: String): User = User(name)
                })

---
+++ src/main/kotlin/me/taesu/demo/chap05/Study05_08_PassLambdaToMethod.kt
@@ -0,0 +1,59 @@
+package me.taesu.demo.chap05
+
+/**
+ * Created by itaesu on 28/05/2020.
+ *
+ * @author Lee Tae Su
+ * @version 1.2.0
+ * @since 1.2.0
+ */
+
+fun main(args: Array<String>) {
+    Study05_08_PassLambdaToMethod().exam()
+    Study05_08_PassLambdaToMethod().exam2()
+}
+
+class Study05_08_PassLambdaToMethod {
+    fun method(name: String, computation: Runnable) {
+        println("""task $name start""")
+        computation.run()
+    }
+
+    //Labmda를 반환하는 메소드는 아래와같이 감싸주어야 한다
+    fun returnLambda(work: String): Runnable = Runnable { println(work) }
+
+    //아래와 같이 선언도 가능
+    val runnalbeLambda: Runnable = Runnable { println("runnable") }
+
+    fun exam() {
+        //객체 식으로 람다를 전달
+        // -> 매번 새로운 무명 객체가 생성된다
+        method("0001", object : Runnable {
+            override fun run() {
+                println("test")
+            }
+        })
+
+        //람다가 별다른 변수를 capture 하고 있지 않으면 전역에서 하나만 생성하여 재사용한다
+        method("00002", Runnable { println("00002 test") })
+
+        //매번 새로운 람다를 생성 함
+        val workName = "work name"
+        method("00002", Runnable { println(workName) })
+
+        method("awefaf", returnLambda("work"))
+        method("awefaf", runnalbeLambda)
+    }
+
+
+    private data class Person(val name: String, val email: String? = "UNKNOWN")
+
+    private fun lambda(name: String, personSupplier: (name: String) -> Person): Person = personSupplier.invoke(name)
+    private fun lambda(name: String, email: String, personSupplier: (name: String, email: String) -> Person): Person = personSupplier.invoke(name, email)
+
+    fun exam2() {
+        println(lambda("taesu", { Person(it) }))
+        println(lambda("taesu", "qwer", { name: String, email: String -> Person(name, email) }))
+    }
+
+}
