01abe6d7c163c836d800b588ac58fc6dd2cce958
Lee Tae Su <taesu@crscube.co.kr>
2021-02-10 11:49:58 +0900

    Kotlin애서 null safety하게 type을 비교해야 할 때

--- src/test/kotlin/me/taesu/demo/exp/NullableAndEqualsTest.kt
+++ src/test/kotlin/me/taesu/demo/exp/NullableAndEqualsTest.kt
@@ -21,7 +21,7 @@ class NullableAndEqualsTest {
     }
 
     @Test
-    fun `When objects are nullable, compare compare result should true`() {
+    fun `When objects are nullable, compare result should true`() {
         fun nullableUser(): User? = User(name = "taesu", email = "taesu@crscube.co.kr")
 
         val user: User? = null
@@ -31,14 +31,36 @@ class NullableAndEqualsTest {
         assertThat(user?.key != null && user.key == user2?.key).isFalse()
     }
 
-     @Test
-    fun `When object is nullable, compare compare result should true`() {
+    @Test
+    fun `When object is nullable, compare result should true`() {
         fun nullableUser(): User? = User(key = 2L, name = "taesu", email = "taesu@crscube.co.kr")
 
         val user: User? = null
         val user2: User? = nullableUser()
 
         assert(user?.key != user2?.key)
+    }
+    
+    @Test
+    fun `When object is nullable, type compare result should true`() {
+        fun nullableUser(): User? = User(key = 2L, name = "taesu", email = "taesu@crscube.co.kr")
+
+        val user: User? = null
+        val user2: User? = nullableUser()
+        val user3: Any? = null
+        val user4: Any? = nullableUser()
+
+        assertThat(user is User).isFalse()
+        assertThat(user2 is User).isTrue()
+        assertThat(user3 is User).isFalse()
+        assertThat(user4 is User).isTrue()
+
+        if (user4 !is User) {
+            return
+        }
+
+        // smart casting
+        println(user4.name)
     }
 }
 
